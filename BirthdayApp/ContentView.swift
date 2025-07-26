a942f86dae7dbfbda371cabaf1b54a40506ad7ba
Diana <diudiana7788@gmail.com>
2025-07-25 18:17:20 -0700

    finished display of friend list

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -15,13 +15,20 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name){ friend in
+                
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+                
+            }
+            .navigationTitle("Birthdays")
+            
         }
-        .padding()
+        
     }
 }
 
