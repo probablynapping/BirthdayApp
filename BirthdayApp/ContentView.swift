bf8f212db16a1ce2d56cbcc04e8bd06c7392e457
Diana <diudiana7788@gmail.com>
2025-07-25 18:04:47 -0700

    created Friend class and an array of friends

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [Friend] = [
+        Friend(n: "Arushi", d: Date(timeIntervalSince1970: 0)),
+        Friend(n: "Deanna", d: .now)
+    ]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ Friend.swift
@@ -0,0 +1,23 @@
+//
+//  Friend.swift
+//  BirthdayApp
+//
+//  Created by D on 7/25/25.
+//
+
+import Foundation
+
+class Friend{
+    
+    //this is how u declare without assigning value (initializing)
+    var name: String
+    var birthday: Date
+    
+    init(n:String, d:Date){
+        name = n
+        birthday = d
+    }
+    
+    
+    
+}
