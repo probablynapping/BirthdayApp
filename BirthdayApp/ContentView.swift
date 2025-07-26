803541656637361f5ddb88f5aff42054daf23d63
Diana <diudiana7788@gmail.com>
2025-07-26 14:47:07 -0700

    lmao erm added an add friend function and went back a bit on the Query part

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -10,8 +10,14 @@ import SwiftData
 
 struct ContentView: View {
     
-    @Query private var friends: [Friend]
+    @State private var friends: [Friend] = [
+        Friend(n: "Elton Lin", d: .now),
+        Friend(n: "Jenny Court", d: Date(timeIntervalSince1970: 0))
+    ]
+//    @Environment(\.modelContext) private var context
     
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack{
@@ -25,6 +31,24 @@ struct ContentView: View {
                 
             }
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                                TextField("Name", text: $newName)
+                                    .textFieldStyle(.roundedBorder)
+                            }
+                    Button("Save") {
+                                let newFriend = Friend(n: newName, d: newBirthday)
+                                friends.append(newFriend)
+                            }
+                            .bold()
+                    }
+                    .padding()
+                    .background(.bar)
+                    
+            }
             
         }
         
